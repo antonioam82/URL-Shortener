06b17b33506e742147b317577844bc4b7ed1b730
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-11-22 17:21:49 +0100

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import pyshorteners
+import pyshorteners as ps
+import threading
 import os
 
 class shortener:
@@ -11,15 +12,27 @@ class shortener:
 
         current_dir = StringVar()
         current_dir.set(os.getcwd())
+        self.url = StringVar()
 
         Label(self.app,text="URL Shortener...",bg="PaleGreen2",fg="white",font=('Arial', 40, 'bold')).place(x=20,y=40)
         Entry(self.app,textvariable=current_dir,width=172).place(x=0,y=0)
-        url_visor=Entry(self.app,width=37,font='Arial, 33')
-        url_visor.place(x=20,y=150)
-        Button(self.app,text="SHORTEN",height=3,width=12).place(x=925,y=150)
+        self.url_visor=Entry(self.app,textvariable=self.url,width=37,font='Arial, 33')
+        self.url_visor.place(x=20,y=150)
+        Button(self.app,text="SHORTEN",height=3,width=12,command=self.init_task).place(x=925,y=150)
         
 
         self.app.mainloop()
+
+    def shorten_URL(self):
+        if self.url_visor.get()!="":
+            url = self.url_visor.get()
+            self.url.set(ps.Shortener().tinyurl.short(url))
+
+    def init_task(self):
+        t = threading.Thread(target=self.shorten_URL)
+        t.start()
+            
+        
 
 if __name__=="__main__":
     shortener()
