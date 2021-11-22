ada9c0c1a6c897e91f63164dd3ba38c9986fd4df
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-11-22 16:32:42 +0100

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -13,6 +13,9 @@ class shortener:
         current_dir.set(os.getcwd())
 
         Entry(self.app,textvariable=current_dir,width=172).place(x=0,y=0)
+        url_visor=Entry(self.app,width=37,font='Arial, 33')
+        url_visor.place(x=20,y=150)
+        Button(self.app,text="SHORTEN",height=3,width=12).place(x=925,y=150)
         
 
         self.app.mainloop()
