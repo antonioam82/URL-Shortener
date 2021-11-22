7f337e7c27946f894af5c03263109aff8cc0a12a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-11-22 16:14:39 +0100

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -1,11 +1,19 @@
 from tkinter import *
 import pyshorteners
+import os
 
 class shortener:
     def __init__(self):
         self.app = Tk()
         self.app.title("URL Shortener")
-        self.app.geometry("990x370")
+        self.app.geometry("1038x370")
+        self.app.configure(background="PaleGreen2")
+
+        current_dir = StringVar()
+        current_dir.set(os.getcwd())
+
+        Entry(self.app,textvariable=current_dir,width=172).place(x=0,y=0)
+        
 
         self.app.mainloop()
 
