8fb8fa428be8310ee38d435f99d3e77ae97fcb6a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-11-23 12:00:14 +0100

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import pyshorteners as ps
 from urllib.parse import urlparse
 import threading
@@ -22,6 +23,9 @@ class shortener:
         self.url_visor=Entry(self.app,textvariable=self.url,width=37,font='Arial, 33')
         self.url_visor.place(x=20,y=150)
         Button(self.app,text="SHORTEN",height=3,width=12,command=self.init_task).place(x=925,y=150)
+        Button(self.app,text="COPY",height=1,width=12).place(x=925,y=235)
+        Button(self.app,text="CLEAR",height=1,width=12,command=self.clear).place(x=819,y=235)
+        Button(self.app,text="IMPORT",height=1,width=12).place(x=713,y=235)
         
 
         self.app.mainloop()
@@ -32,6 +36,12 @@ class shortener:
             url = self.url_visor.get()
             self.url.set(ps.Shortener().tinyurl.short(url))
             self.ShrtStatus.configure(text="Task completed :)")
+        else:
+            messagebox.showwarning("INVALID URL","Enter a valid URL.")
+
+    def clear(self):
+        self.url.set("")
+        self.ShrtStatus.configure(text="")
 
     def is_url(self,url):
         try:
